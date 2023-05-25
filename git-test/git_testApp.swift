a9266eeafbf8e8908e56c47da760caca23065cdb
Aditya Raj Singh <adi4adityaraj@gmail.com>
2023-05-25 10:56:51 +0530

    Initial Commit

---
+++ git-test/git_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  git_testApp.swift
+//  git-test
+//
+//  Created by Aditya Raj Singh on 25/05/23.
+//
+
+import SwiftUI
+
+@main
+struct git_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
